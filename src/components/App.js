9049e05859c9c26e79b04af4c563255672c00af2
anahita24 <75325348+anahita24@users.noreply.github.com>
2021-02-22 21:12:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,25 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+  setX(0);
+  setY(0);
+    setBallPosition({left:"0px"});
+    setBallPosition({top:"0px"});
+  };
+  const renderChoice = () => {
+    window.addEvenListener("keydown",(event)+>{
+      if(event.keyCode=='37' || event.key=='LeftArrow')
+      setX(x-5);
+     else if(event.keyCode=='38' || event.key=='UpArrow')
+      setY(y+5);
+      else if(event.keyCode=='39' || event.key=='RightArrow')
+      setX(x+5);
+      else if(event.keyCode=='40' || event.key=='DownArrow')
+      setY(y+5);
+      setBallPosition({left:x+"px", top:y+"px"});
+    }); 
+  };
 
   return (
     <div className="playground">
