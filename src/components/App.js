3c65676f5eddafe4076a0925340001df7ff1cc1d
anahita24 <75325348+anahita24@users.noreply.github.com>
2021-02-23 09:50:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,66 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
-  const [posi, setPosi] = useState({ left: 5, top: 5 });
-  const [ballPosition, setBallPosition] = useState({ left: 0, top: 0 });
-  const updateXY =(x,y)=>{
-      setBallPosition(
-          {
-              left:x,
-              top:y,
-          }
-      )
-
-  }
-  const handleListener=(event)=>{
-    switch (event.key) {
-        case "ArrowRight":
-            updateXY(ballPosition.left + 5,ballPosition.top);
-          break;
-        case "ArrowDown":
-            updateXY(ballPosition.left ,ballPosition.top+5);
-          break;
-        case "ArrowLeft":
-            updateXY(ballPosition.left - 5,ballPosition.top);
-          break;
-        case "ArrowUp":
-            updateXY(ballPosition.left ,ballPosition.top-5);
-          break;
-        default:
-          break;
-      }
-
-  }
-
-  // bind ArrowRight keydown event
-  useEffect(() => {
-    const keyListener= document.addEventListener("keydown", handleListener);
-    return ()=> document.removeEventListener("keydown",handleListener);
-  },[ballPosition]);
-
-  const buttonClickHandler = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [ballPosition,setBallPosition] = useState({
+    left: "0px",
+    top: "0px",
+  });
+  const reset = () => {};
+  const removebutton=()=>{
     setRenderBall(true);
+    renderChoice();
   };
-  const renderBallOrButton = () => {
-    if (renderBall) {
-      return (
-        <div
-          className="ball"
-          style={{
-            left: ballPosition.left + "px",
-            top: ballPosition.top + "px",
-            position: "absolute",
-          }}
-        ></div>
-      );
-    } else {
-      return <button onClick={buttonClickHandler}>Click For One Ball</button>;
-    }
-  };
+  const renderChoice = () => {
+  if(renderBall)
+  return (<div className="ball"></div>);
+  else
+  return (<button className="ballProvider" onClick={removebutton}>Start</button>)
+ };
 
-  return <div className="playground">{renderBallOrButton()}</div>;
+  return (
+    <div className="playground">
+      <button onClick={reset} className="reset">
+        Reset
+      </button>
+      {renderChoice()}
+    </div>
+  );
 };
 
 export default App;
+ 12 
