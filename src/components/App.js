50630f961f4f7085b165a8a99508875526f719fe
anahita24 <75325348+anahita24@users.noreply.github.com>
2021-02-23 10:15:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -35,11 +35,11 @@ const App = () => {
     document.addEventListener("keydown",(event)=>{
         if(event.key=="ArrowLeft"||event.keyCode=='37')
         x=x-5;
-       else if(event.key=="ArrowUp"||event.keyCode=='38' )
+        if(event.key=="ArrowUp"||event.keyCode=='38' )
         y=y+5;
-        else if( event.key=="ArrowRight"||event.keyCode=='39' )
+        if(event.key=="ArrowRight"||event.keyCode=='39' )
         x=x+5;
-        else if(event.key=="ArrowDown"||event.keyCode=='40' )
+        if(event.key=="ArrowDown"||event.keyCode=='40' )
         y=y-5;
         setBallPosition({left: x+"px",top: y+"px"});
         console.log(event.key+" "+x+" "+y);
